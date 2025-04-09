8567e40dfaf6967d859916964e2f689b703e3be1
AdelJesus <jusayuivan23@gmail.com>
2025-04-09 00:07:17 -0500

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -929,7 +929,3 @@ def get_notificaciones_cliente():
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
